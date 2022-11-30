0d2067428659ec4ee8b5c9290d092e1cfcee2fb8
AHStovall <adamhstovall@gmail.com>
2022-11-30 00:10:41 -0500

    Updated logic for confirmation, prompts, random number generator, updated variable

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,17 @@
 // Assignment code here
+// var passwordCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
 var passwordCharUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordCharLower = "abcdefghijklmnopqrstuvwxyz"
+var passwordCharLower = "abcdefghijklmnopqrstuvwxyz";
 var passwordCharNum = "1234567890";
 var passwordCharSpecial = "!@#$%^&*";
+var password = " ";
 var charLength = {
   from: 8,
   to: 128
 };
 
-function randomNumGen() {
+//random number generator
+function randomNumGen(passwordCharSet) {
   for (var i = 0; i <= passwordLength; i++) {
     var randomNum = Math.floor(Math.random()*passwordCharSet.length);
     password += passwordCharSet.substring(randomNum, randomNum+1);
@@ -21,17 +24,34 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   //var password = generatePassword();
+
+  //variables outlining prompt and confirmation pop-ups
+  var lowercaseLetters = confirm("Would you like lower case letters?");
   var uppercaseLetters = confirm("Would you like upper case letters?");
+  var NumChars = confirm("Would you like numbers?")
   var specialChars = confirm("Would you like special characters?")
   var charLength = prompt("Please enter the number of characters you would like for your password.\n\nPlease enter a number between 8 and 128.")
   var passwordText = document.querySelector("#password");
-  // uppercaseLetters.setAttribute("upperCase");
-  // specialChars.setAttribute("specialChars");
-  // charLength.setAttribute("charLength");
+  var passwordcharacters = "";
   
-  if (uppercaseLetters === 'true' && specialChars === 'true') {}
   
-
+  //Logic dictating responses and inputting into the random number generator
+  if (uppercaseLetters === 'true'){
+    passwordcharacters += passwordCharUpper
+  }
+  if (lowercaseLetters === 'true'){
+    passwordcharacters += passwordCharLower
+  }
+  if (NumChars === 'true'){
+    passwordcharacters += passwordCharNum
+  }
+  if (specialChars === 'true'){
+    passwordcharacters += passwordCharSpecial
+  }
+  while(charLength<8 || charLength>128){
+    charLength =prompt("Please enter the number of characters you would like for your password.\n\nPlease enter a number between 8 and 128.")
+  }
+  randomNumGen(passwordcharacters);
   passwordText.value = password;
 
 }
