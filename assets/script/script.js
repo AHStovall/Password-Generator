b839f1d54d1d219bed6cafa09b2097d46dad9c59
AHStovall <adamhstovall@gmail.com>
2022-11-29 23:05:02 -0500

    Updated for review

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,12 @@
 // Assignment code here
-var passwordCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+var passwordCharUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordCharLower = "abcdefghijklmnopqrstuvwxyz"
+var passwordCharNum = "1234567890";
+var passwordCharSpecial = "!@#$%^&*";
 var charLength = {
   from: 8,
   to: 128
 };
-
-function displayParameters() {
-  var upperCase = querySelector
-  var lowerCase  
-
-}
 
 function randomNumGen() {
   for (var i = 0; i <= passwordLength; i++) {
@@ -23,8 +20,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
+  var uppercaseLetters = confirm("Would you like upper case letters?");
+  var specialChars = confirm("Would you like special characters?")
+  var charLength = prompt("Please enter the number of characters you would like for your password.\n\nPlease enter a number between 8 and 128.")
   var passwordText = document.querySelector("#password");
+  // uppercaseLetters.setAttribute("upperCase");
+  // specialChars.setAttribute("specialChars");
+  // charLength.setAttribute("charLength");
+  
+  if (uppercaseLetters === 'true' && specialChars === 'true') {}
+  
 
   passwordText.value = password;
 
